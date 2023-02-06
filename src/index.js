4df042ac149baba100fdd2f4fcc0c2f8ac0298c2
Bete.mrt <bete.mrt@gmail.com>
2023-02-06 18:46:48 -0300

    1º requisito

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs').promises;
+const path = require('path');
 const express = require('express');
 
 const app = express();
@@ -5,6 +7,8 @@ app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+const dataPath = path.resolve(__dirname, './talker.json');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -14,3 +18,31 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online XD');
 });
+
+const readData = async () => {
+  try {
+    const data = await fs.readFile(dataPath, 'utf-8');
+    const response = await JSON.parse(data);
+    return response;
+
+  } catch (error) {
+    console.error(`Erro ao ler o arquivo: ${error.message}`);
+  }
+}
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const data = await readData();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+
+// app.get('/chocolates', async (_req, res) => {
+//   const chocolates = await cacauTrybe.getAllChocolate();
+//   res.status(200).json({ chocolates });
+// });
+
+
